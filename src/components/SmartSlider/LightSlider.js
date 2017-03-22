eb8f1d40a3527d6ba5214e675d4db3b3cd658c14
刘培超 <liupeichao@cmhi.chinamoible.com>
2017-03-22 09:49:14 +0800

    初步版本

---
+++ src/components/SmartSlider/LightSlider.js
@@ -0,0 +1,29 @@
+/**
+ * File Created by lpcma at 2017/3/21.
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ *
+ *
+ * @Desc
+ * @author lpcma
+ * @date 2017/3/21
+ * @version
+ */
+
+import React from 'react';
+import Slider from 'rc-slider/lib/Slider';
+import 'rc-slider/assets/index.css';
+import './LightSlider.scss';
+
+export const LightSlider = (props) => (
+  <div className="smart-slider">
+    <Slider  {...props} className='light-slider'/>
+  </div>
+);
+
+export default LightSlider;
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import SmartSlider, {SliderType} from '../../../components/SmartSlider';
 import DuckImage from '../assets/Duck.jpg';
 import './HomeView.scss';
 
-export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
-  </div>
-);
+class HomeView extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      sliderValue: this.props.defaultValue || 20
+    };
+
+    this.handleSlider = this.handleSlider.bind(this);
+  }
+
+  handleSlider(value){
+
+    this.setState({sliderValue: value});
+  }
+
+  render(){
+    return (
+      <div>
+        <h4>Welcome! {this.state.sliderValue}</h4>
+        <img
+          alt='This is a duck, because Redux!'
+          className='duck'
+          src={DuckImage}/>
+        <SmartSlider
+          type={SliderType.LIGHT}
+          min={0}
+          max={100}
+          defaultValue={this.state.sliderValue}
+          onChange={this.handleSlider}
+        />
+      </div>
+    );
+  }
+}
 
 export default HomeView;
