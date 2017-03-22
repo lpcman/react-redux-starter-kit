cbd76c7a83b22bfd0e2f13ba8fad6791a48a0986
hejunli <hejunli@cmhi.chinamobile.com>
2017-03-22 09:55:53 +0800

    btn组件

---
+++ src/components/OptionBtn/index.js
@@ -0,0 +1,19 @@
+/**
+ * File Created by cherish at 2017-03-21.
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ *
+ *
+ * @Desc
+ * @author cherish
+ * @date 2017-03-21
+ * @version
+ */
+import OptionBtn from './OptionBtn';
+
+export default OptionBtn;
---
+++ src/components/OptionBtn/setting.js
@@ -0,0 +1,44 @@
+/**
+ * File Created by cherish at 2017-03-21.
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ *
+ *
+ * @Desc
+ * @author cherish
+ * @date 2017-03-21
+ * @version
+ */
+import controlImg  from './assets/control@2x.png';
+import discontrolImg  from './assets/discontrol@2x.png';
+import chartImg  from './assets/chart@2x.png';
+import timerImg  from './assets/timer@2x.png';
+const Enum = {
+  control: {
+    type: "control",
+    bgImage: controlImg,
+    redirectTo: '/',
+  },
+  discontrol: {
+    type: "discontrol",
+    bgImage: discontrolImg,
+    redirectTo: '/',
+  },
+  chart: {
+    type: "chart",
+    bgImage: chartImg,
+    redirectTo: '/',
+  },
+  timer: {
+    type: "timer",
+    bgImage: timerImg,
+    redirectTo: '/',
+  }
+
+};
+exports.default = Enum;
