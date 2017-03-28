100ca392acd4ba0491652a5fa03c10f867171514
刘培超 <liupeichao@cmhi.chinamoible.com>
2017-03-28 16:10:24 +0800

    add Toast componet.

---
+++ src/components/Toast/index.js
@@ -0,0 +1,21 @@
+/**
+ * File Created by lpcma at 2017/3/28.
+ * Copyright 2016 CMCC Corporation Limited.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * ZYHY Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license.
+ *
+ *
+ * @Desc
+ * @author lpcma
+ * @date 2017/3/28
+ * @version
+ */
+import Notifications, {notify}  from './Notify';
+
+
+export { notify };
+export default Notifications;
